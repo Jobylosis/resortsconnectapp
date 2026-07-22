26fd8664b3c1a7ddc7f1c8b84176960c27fff7d8
Jobylosis <deinieljobramos@gmail.com>
2026-07-23 00:09:45 +0800

    server fix

--- start_servers.bat
+++ start_servers.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Starting Python OCR Server...
-start cmd /k "cd c:\Users\PC\GithubRepo\resortsconnectapp\ocr_server && uvicorn main:app --host 0.0.0.0 --port 8000"
+start cmd /k "cd c:\Users\PC\GithubRepo\resortsconnectapp-main\ocr_server && uvicorn main:app --host 0.0.0.0 --port 8000"
 
 echo Starting Ngrok Tunnel...
-start cmd /k "cd c:\Users\PC\GithubRepo\resortsconnectapp && ngrok http --url walk-versus-peculiar.ngrok-free.dev 8000"
+start cmd /k "cd c:\Users\PC\GithubRepo\resortsconnectapp-main && ngrok http --url walk-versus-peculiar.ngrok-free.dev 8000"
 
 echo Both servers have been started in separate windows!
 exit
